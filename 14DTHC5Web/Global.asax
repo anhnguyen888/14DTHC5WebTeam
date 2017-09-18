9c42380e4eb88fa49957e0b615e8ff65983fbee2
dinhanh <nd.anh@hutech.edu.vn>
2017-09-18 15:15:50 +0700

    Add project files.

---
+++ 14DTHC5Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_14DTHC5Web.MvcApplication" Language="C#" %>
---
+++ 14DTHC5Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_14DTHC5Web.Startup))]
+namespace _14DTHC5Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
